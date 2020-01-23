91f01e7dd5a4d1301d66127266a257f6113fabdb
Ahir <karan.ahir@accenture.com>
2020-01-23 19:28:34 +0530

    created the shop page and related components

--- src/App.js
+++ src/App.js
@@ -3,18 +3,15 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+
 
 function App() {
   return (
     <div>
       <Route exact path="/" component={HomePage}/>
-      <Route exact path="/hats" component={HatPage}/>
+      <Route exact path="/shop" component={ShopPage}/>
     </div>
   );
 }
