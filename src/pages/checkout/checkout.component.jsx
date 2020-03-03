65ad70e6a97ddd47752b84311ba296ba486e5efb
Ahir <karan.ahir@accenture.com>
2020-03-03 17:03:21 +0530

    created a reducer for shop data and wrote the shop selectors and also created shop/directory redirection

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -6,7 +6,7 @@ import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selector
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
 import './checkout.styles.scss';
-import CartItem from '../../components/cart-item/cart-item.component';
+//import CartItem from '../../components/cart-item/cart-item.component';
 
 const CheckoutPage = ({ cartItems, total }) => (
     <div className="checkout-page">
