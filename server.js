831b03c50f4253883dc9a2bee62e75d8759491c2
Ahir <karan.ahir@accenture.com>
2020-04-10 19:15:16 +0530

    Added the Server with express and created a client folder to separate the client

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+
+if(process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());    
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+
+    app.get('*', function(request, response) {
+        response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+
+app.listen(port, error => {
+    if(error) throw error;
+    console.log("Server running on port " + port);
+});
+
+app.post('/payment', (request, response) => {
+    console.log("This is the request =============================" + request);
+    const body = {
+        source: request.body.token.id,
+        amount: request.body.amount,
+        currency: 'usd'
+    };
+
+    stripe.charges.create(body, (stripeErr, stripeRes) => {
+        console.log("Inside Create charge method.");
+        if(stripeErr){
+            response.status(500).send({ error: stripeErr});
+        }
+        else {
+            response.status(200).send({ success: stripeRes});
+        }    
+    });
+});
