c3312e7b6cace664dd9e5d693f8498d27a70a153
Ahir <karan.ahir@accenture.com>
2020-08-12 19:19:07 +0530

    Converted app to PWA

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const { response } = require('express');
 
 
 if(process.env.NODE_ENV !== 'production') require('dotenv').config();
@@ -28,6 +29,10 @@ if(process.env.NODE_ENV === 'production') {
 app.listen(port, error => {
     if(error) throw error;
     console.log("Server running on port " + port);
+});
+
+app.get('./service-worker.js', (request, response) => {
+    response.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
 });
 
 app.post('/payment', (request, response) => {
